cca0d964a55385d46aa5032f3390fafb0fd1e645
Никита Буховец <nihtoe@yandex.ru>
2023-10-22 19:44:25 +0300

    Пофиксил все ошибки2!

--- homework.py
+++ homework.py
@@ -18,14 +18,13 @@ class InfoMessage:
         self.calories = calories
 
     def get_message(self) -> str:
-        message_template: str = (
+        return (
             f'Тип тренировки: {self.training_type}; '
             f'Длительность: {self.duration:.3f} ч.; '
             f'Дистанция: {self.distance:.3f} км; '
             f'Ср. скорость: {self.speed:.3f} км/ч; '
             f'Потрачено ккал: {self.calories:.3f}.'
         )
-        return message_template
 
 
 class Training:
@@ -33,7 +32,6 @@ class Training:
     LEN_STEP: float = 0.65
     M_IN_KM: int = 1000
     M_IN_HOUR: int = 60
-    training_type = None
 
     def __init__(self,
                  action: int,
@@ -43,7 +41,6 @@ class Training:
         self.action = action
         self.duration = duration
         self.weihgt = weight
-        self.calories = self.get_spent_calories()
 
     def get_distance(self) -> float:
         """Получить дистанцию в км."""
@@ -58,12 +55,12 @@ class Training:
 
     def show_training_info(self) -> InfoMessage:
         """Вернуть информационное сообщение о выполненной тренировке."""
-        info = InfoMessage(type(self).__name__,
+        return InfoMessage(type(self).__name__,
                            self.duration,
                            self.get_distance(),
                            self.get_mean_speed(),
-                           self.calories)
-        return info
+                           self.get_spent_calories())
+
 
 
 class Running(Training):
